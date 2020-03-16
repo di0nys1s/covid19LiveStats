f0b4d3e6039a7b14bbf58f20e54627c345467f52
Burak Seyhan <seyhanburak9@gmail.com>
2020-03-16 16:51:39 +1100

    form complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Header from "./components/Header";
+import Stats from "./components/Stats";
 import "./App.css";
 
 function App() {
-  return <h1>Hello Covid-20 Followers</h1>;
+  return (
+    <div className="container">
+      <Header country="Turkey" />
+      <Stats />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 class="ui header">Covid19 Statistics</h1>
+    </div>
+  );
+};
+
+export default Header;
